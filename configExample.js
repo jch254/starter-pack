d091a04b67fc7f7cec02abb06dd1279f818a0e7c
Jordan Hornblow <jordan@jch254.com>
2016-05-24 01:41:46 +1000

    Add Google Analytics

--- configExample.js
+++ configExample.js
@@ -2,4 +2,5 @@ module.exports = {
   'process.env.NODE_ENV': JSON.stringify('development'),
   'process.env.AUTH0_CLIENT_ID': JSON.stringify('YOUR AUTH0 CLIENT ID'),
   'process.env.AUTH0_DOMAIN': JSON.stringify('YOUR AUTH0 DOMAIN'),
+  'process.env.GA_ID': JSON.stringify('YOUR GOOGLE ANALYTICS ID'),
 };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory, applyRouterMiddleware } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import useScroll from 'react-router-scroll';
+import ga from 'react-ga';
 
 import configureStore from './configureStore';
 import routes from './routes';
@@ -12,12 +13,23 @@ import routes from './routes';
 const store = configureStore(browserHistory);
 const history = syncHistoryWithStore(browserHistory, store);
 
+if (process.env.NODE_ENV === 'production') {
+  ga.initialize(process.env.GA_ID);
+}
+
+function logPageView() {
+  if (process.env.NODE_ENV === 'production') {
+    ga.pageview(window.location.pathname);
+  }
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <Router
       history={history}
       routes={routes}
       render={applyRouterMiddleware(useScroll())}
+      onUpdate={logPageView}
     />
   </Provider>,
   document.getElementById('root')
