fd66541d3bca1e8e5e9b0ff0e6a95f7ee9d18cce
Jordan Hornblow <jordan@jch254.com>
2016-10-31 01:07:40 +1100

    Tidy bash scrips

--- build-artifacts.bash
+++ build-artifacts.bash
@@ -1,6 +1,5 @@
+#!/bin/bash -ex
+
 yarn install
 export NODE_ENV=production
-export AUTH0_CLIENT_ID=$AUTH0_CLIENT_ID
-export AUTH0_DOMAIN=$AUTH0_DOMAIN
-export GA_ID=$GA_ID
 yarn run build
--- upload-artifacts.bash
+++ upload-artifacts.bash
@@ -1,7 +1,7 @@
-cd infrastructure
-S3_BUCKET_ID=`terraform output s3_bucket_id`
-CLOUDFRONT_DISTRIBUTION_ID=`terraform output cloudfront_distribution_id`
-cd ..
+#!/bin/bash -ex
+
+S3_BUCKET_ID=$(cd infrastructure && terraform output s3_bucket_id)
+CLOUDFRONT_DISTRIBUTION_ID=$(cd infrastructure && terraform output cloudfront_distribution_id)
 
 cd dist
 aws s3 sync . "s3://${S3_BUCKET_ID}/" --delete --acl=public-read --exclude '.git/*'
