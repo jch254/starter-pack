483f16a3f446fe5b508dbc35547894082190ac49
Jordan Hornblow <jordan@jch254.com>
2025-09-19 16:44:50 +0700

    Update DNS

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,7 +1,7 @@
 output "s3_bucket_id" {
-  value = "${module.webapp.s3_bucket_id}"
+  value = module.webapp.s3_bucket_id
 }
 
 output "cloudfront_distribution_id" {
-  value = "${module.webapp.cloudfront_distribution_id}"
+  value = module.webapp.cloudfront_distribution_id
 }
