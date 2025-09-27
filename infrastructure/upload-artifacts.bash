b4b925b5abb15b85cd2468ba86031dacb0d32f5d
Jordan Hornblow <jordan@jch254.com>
2025-09-28 00:19:25 +0700

    Fix upload script to use terraform output -raw to avoid quoted values

--- infrastructure/upload-artifacts.bash
+++ infrastructure/upload-artifacts.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
-S3_BUCKET_ID=$(cd infrastructure && terraform output s3_bucket_id)
-CLOUDFRONT_DISTRIBUTION_ID=$(cd infrastructure && terraform output cloudfront_distribution_id)
+S3_BUCKET_ID=$(cd infrastructure && terraform output -raw s3_bucket_id)
+CLOUDFRONT_DISTRIBUTION_ID=$(cd infrastructure && terraform output -raw cloudfront_distribution_id)
 
 cd dist
 aws s3 sync . "s3://${S3_BUCKET_ID}/" --delete --acl=public-read --exclude '.git/*'
